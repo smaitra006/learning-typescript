10037c7ab4c7084997acd65dd892d6bbd30ff3e3
Subhrajit <smaitra006@gmail.com>
2025-06-30 19:28:23 +0530

    Add typescript learning codes to github

---
+++ 6. Union Types/1. Union.ts
@@ -0,0 +1,25 @@
+let password: string | number = 20;
+
+type UserInfo = {
+  first: string;
+  last: string;
+  age: number;
+};
+
+type AccountDetails = {
+  email: string;
+  password: string;
+};
+
+let user: UserInfo | AccountDetails = {
+  first: "HuXn",
+  last: "WebDev",
+  age: 29,
+  email: "smaitra006"
+};
+
+console.log(user);
+
+const items: (number | string)[] = [1, 3, "hello"];
+console.log(items);
+
